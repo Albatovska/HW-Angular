c847063e0c567783cfd50766959a3968b1a5d5cb
albatovska <tanya.goncharenko@gmail.com>
2019-07-22 10:59:16 +0300

    change HW4

--- homeWork4/app.js
+++ homeWork4/app.js
@@ -1,54 +1,46 @@
-var _a = require('rxjs'), create = _a.Observable.create, range = _a.range, interval = _a.interval;
-var _b = require('rxjs/operators'), reduce = _b.reduce, bufferCount = _b.bufferCount, bufferTime = _b.bufferTime, merge = _b.merge;
+var _a = require('rxjs'), create = _a.Observable.create, range = _a.range, interval = _a.interval, timer = _a.timer;
+var _b = require('rxjs/operators'), reduce = _b.reduce, bufferCount = _b.bufferCount, bufferTime = _b.bufferTime, buffer = _b.buffer, merge = _b.merge;
 //1. Имплементировать функцилнал range используя функцию create.
-/* const publisher = create((observable) => {
-    let i = 3;
-    let count= i+ 10;
-    for(i ; i < count; i++ ){
-    
-    observable.next(i);
-    
+var publisherRange = create(function (observable) {
+    var i = 3, count = i + 10;
+    for (i; i <= count; i++) {
+        observable.next(i);
     }
-}); */
+});
+var subscriberFirst = publisherRange.subscribe(function (val) {
+    console.log("This is observable value: " + val);
+});
 //2. Имплементировать функционал interval используя функцию create.
-/* const publisher = create((observable) => {
-    let count= 0;
-    let i = 1;
-    const interval = setInterval(() =>{
-    
-    observable.next(`This is next interation ${i}`);
-    i++;
-     if (i > 5) {
+var publisherInterval = create(function (observable) {
+    var timeInterval = 500;
+    var i = 0;
+    setInterval(function () {
+        observable.next(i);
+        i++;
+        if (i > 5) {
             clearInterval(interval);
         }
-    }, 500)
+    }, timeInterval);
 });
- */
+var subscriberSecond = publisherInterval.subscribe(function (val) {
+    console.log("This is observable value: " + val);
+});
 //3. Используя тольк reduce иммплементировать функционал filter.
-/* const publisher = range (0, 100)
-.pipe(reduce((acc, val) => {
-
- if(val % 3 === 0 ) {
-      acc = val;
-   console.log(acc)
-      
-  }
- 
-  return acc
-   
- 
-}, 10))
-
-*/
+var publisherFilter = range(0, 100)
+    .pipe(reduce(function (acc, val) {
+    if (val % 3 === 0) {
+        acc += val;
+        console.log(acc);
+    }
+    return acc;
+}, 10));
+var subscriberThird = publisherFilter.subscribe(function (val) {
+    console.log("This is observable value: " + val);
+});
 //4. Дан обзервабле interval - 1 tick in 50 msec -> выдавать данные либо 1 раз в 333 сек или же когда накапливается 7 элементов.
-var publisher1 = interval(100).pipe(bufferTime(333));
-var publisher2 = interval(100).pipe(bufferCount(7));
-var publisher = publisher1.pipe(merge(publisher2));
+var publisher = interval(500)
+    .pipe(bufferCount(7))
+    .pipe(bufferTime(3330));
 var subscriber = publisher.subscribe(function (val) {
     console.log("This is observable value: " + val);
-}, function (err) {
-    console.log("Error appears " + err.message);
-}, function () {
-    console.log('FINISHED');
 });
-setTimeout(function () { return subscriber.unsubscribe(); }, 5000);
--- homeWork4/app.ts
+++ homeWork4/app.ts
@@ -1,64 +1,74 @@
+
 const { Observable: { create }, range, interval, timer} = require('rxjs');
 const {reduce, bufferCount, bufferTime, buffer, merge}= require('rxjs/operators');
 
 //1. Имплементировать функцилнал range используя функцию create.
-/* const publisher = create((observable) => {
-    let i = 3;
-    let count= i+ 10;
-    for(i ; i < count; i++ ){
+
+  const publisherRange = create((observable:any) => {
+   let i: number = 3,
+   count: number = i + 10;
+    for(i ; i <= count; i++ ){
     
     observable.next(i);
     
     }
-}); */
+}); 
+const subscriberFirst = publisherRange.subscribe(
+    (val:number) => {
+        console.log(`This is observable value: ${val}`);
+    }) 
+
+
 //2. Имплементировать функционал interval используя функцию create.
-/* const publisher = create((observable) => {
-    let count= 0;
-    let i = 1;
-    const interval = setInterval(() =>{
+
+  const publisherInterval = create((observable:any) => {
+     const timeInterval = 500;
+    let i: number = 0;
+       setInterval(() =>{
     
-    observable.next(`This is next interation ${i}`);
-    i++;
-     if (i > 5) {
+    observable.next(i);
+     i++;
+      if (i > 5) {
             clearInterval(interval);
         }
-    }, 500)
-});
- */
+      }, timeInterval);
+    });
+const subscriberSecond = publisherInterval.subscribe(
+    (val:number) => {
+        console.log(`This is observable value: ${val}`);
+    });
+  
+
 //3. Используя тольк reduce иммплементировать функционал filter.
-  const publisher = range (0, 100)
- .pipe(reduce((acc, val) => {
+
+ const publisherFilter = range (0, 100)
+ .pipe(reduce((acc: number, val: number) => {
 
   if(val % 3 === 0 ) {
        acc += val; 
     console.log(acc)
        
    }
-  
+
    return acc
     
   
  }, 10))
 
+ const subscriberThird = publisherFilter.subscribe(
+    (val:number) => {
+        console.log(`This is observable value: ${val}`);
+    });
 
- 
 
 //4. Дан обзервабле interval - 1 tick in 50 msec -> выдавать данные либо 1 раз в 333 сек или же когда накапливается 7 элементов.
 
- /* const publisher = timer(50, 1)
-.pipe(bufferTime(333, 1, 7))
- */
+ const publisher = interval(500)
+ .pipe(bufferCount(7))
+.pipe(bufferTime(3330));
+ 
 const subscriber = publisher.subscribe(
-    (val) => {
-        console.log(`This is observable value: ${val}`);
-    },
-    (err) => {
-        console.log(`Error appears ${err.message}`);
-    },
-    () => {
-        console.log('FINISHED')
-     
-    }
-    );
-    setTimeout(() => subscriber.unsubscribe(), 5000);
-  
+    (val: number) => {
+        console.log(`This is observable value: ${val}`)
+    });
+ 
